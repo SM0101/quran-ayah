5b5e13d48d787dc6a3058f4fe07091265a59bbfd
SM <m@user.com>
2024-11-28 13:01:24 +0600

    used multiple api

--- src/app/components/pages/pages.component.ts
+++ src/app/components/pages/pages.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, HostListener, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { QuranApiServiceService } from '../../services/quran-api-service.service';
+import { forkJoin } from 'rxjs';
 //import { ApiResponse } from '../../models/interfaces/ayah';
 
 @Component({
@@ -14,6 +15,8 @@ import { QuranApiServiceService } from '../../services/quran-api-service.service
 export class PagesComponent implements OnInit {
   //ayahs: ApiResponse[] =[];
   ayahs: any[] = [];
+  arabic: any[] = [];
+  english: any[] = [];
 
   surahNumbers: number = 1;
   ayahNumber: number = 1;
@@ -27,86 +30,93 @@ export class PagesComponent implements OnInit {
   }
 
   loadAyahs() {
-    this.quranService.getAyah( this.surahNumbers,this.ayahNumber).subscribe((response) => {
-      this.ayahs = [{
-        surahName: response.data.surah.englishName,
-        surahNameArabic: response.data.surah.name,
-        arabic2: response.data.text
-        // surahName: response.surahName,
-        // surahNameArabic: response.surahNameArabic,
-        // english: response.english,
-        // arabic1: response.arabic1,
-        // arabic2: response.arabic2
-      }];
-      //this.totalAyahs = response.totalAyah;
-      this.totalAyahs = response.data.surah.numberOfAyahs;
+    forkJoin({
+      arabic: this.quranService.getAyah(this.surahNumbers, this.ayahNumber),
+      english: this.quranService.getAyahTranslation(this.surahNumbers, this.ayahNumber)
+    }).subscribe({
+      next: (response) => {
+        this.arabic = response.arabic;
+        this.english = response.english;
+
+        this.ayahs = [{
+          surahName: response.arabic.data.surah.englishName,
+          surahNameArabic: response.arabic.data.surah.name,
+          arabic2: response.arabic.data.text,
+          english: response.english.english
+        }];
+        this.totalAyahs = response.arabic.data.surah.numberOfAyahs;
+
+      },
+      error: (err) => {
+        console.error('Error fetching data:', err);
+      }
     });
   }
 
-  search() {
-    this.resetAyahs();
-    this.loadAyahs();
-  }
+search() {
+  this.resetAyahs();
+  this.loadAyahs();
+}
 
-  resetAyahs() {
-    this.ayahs = [];
-    this.ayahNumber = this.ayahNumber;
-  }
+resetAyahs() {
+  this.ayahs = [];
+  this.ayahNumber = this.ayahNumber;
+}
 
-  searchPrev() {
-    if (this.ayahNumber > 1) {
-      this.loadPrevAyah();
-    } else {
-      this.surahNumbers != 1 ? this.surahNumbers-- : this.surahNumbers = 1;
-      this.ayahNumber = this.totalLastAyah;
-      this.loadPrevAyah();
-    }
+searchPrev() {
+  if (this.ayahNumber > 1) {
+    this.loadPrevAyah();
+  } else {
+    this.surahNumbers != 1 ? this.surahNumbers-- : this.surahNumbers = 1;
+    this.ayahNumber = this.totalLastAyah;
+    this.loadPrevAyah();
   }
+}
 
-  searchNext() {
-    if (this.ayahNumber < this.totalAyahs) {
-      this.loadNextAyah();
-    }
-    else if (this.surahNumbers < 114) {
-      this.surahNumbers++;
-      this.ayahNumber = 0;
-      //this.ayahNumber = 1;
-      this.loadNextAyah();
-      this.totalLastAyah = this.totalAyahs + 1;
-    }
-    else {
-      console.log('Already at the last Ayah. No next Ayah to load.');
-    }
+searchNext() {
+  if (this.ayahNumber < this.totalAyahs) {
+    this.loadNextAyah();
   }
-  loadNextAyah() {
-    if (this.ayahNumber < this.totalAyahs) {
-      this.ayahNumber++; // Increment to load the next Ayah
-      this.loadAyahs(); // Fetch and load the next Ayah
-      console.log(`loadnext ayah${this.ayahNumber}`);
-      console.log(`loadnext surah${this.surahNumbers}`);
-    } else {
-      console.log('No more Ayahs to load.');
-    }
+  else if (this.surahNumbers < 114) {
+    this.surahNumbers++;
+    this.ayahNumber = 0;
+    //this.ayahNumber = 1;
+    this.loadNextAyah();
+    this.totalLastAyah = this.totalAyahs + 1;
   }
+  else {
+    console.log('Already at the last Ayah. No next Ayah to load.');
+  }
+}
+loadNextAyah() {
+  if (this.ayahNumber < this.totalAyahs) {
+    this.ayahNumber++; // Increment to load the next Ayah
+    this.loadAyahs(); // Fetch and load the next Ayah
+    console.log(`loadnext ayah${this.ayahNumber}`);
+    console.log(`loadnext surah${this.surahNumbers}`);
+  } else {
+    console.log('No more Ayahs to load.');
+  }
+}
 
-  loadPrevAyah() {
-    if (this.ayahNumber > 1) {
-      this.ayahNumber--; // Decrement to load the previous Ayah
-      this.loadAyahs(); // Fetch and load the previous Ayah
-    } else {
-      console.log('No more previous Ayahs to load.');
-    }
+loadPrevAyah() {
+  if (this.ayahNumber > 1) {
+    this.ayahNumber--; // Decrement to load the previous Ayah
+    this.loadAyahs(); // Fetch and load the previous Ayah
+  } else {
+    console.log('No more previous Ayahs to load.');
   }
+}
 
 
-  @HostListener('window:keydown', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
-    if (event.key === 'ArrowRight') {
-      this.searchNext();
-    } else if (event.key === 'ArrowLeft') {
-      this.searchPrev();
-    }
+@HostListener('window:keydown', ['$event'])
+handleKeyboardEvent(event: KeyboardEvent) {
+  if (event.key === 'ArrowRight') {
+    this.searchNext();
+  } else if (event.key === 'ArrowLeft') {
+    this.searchPrev();
   }
+}
 
 
 }
--- src/app/services/quran-api-service.service.ts
+++ src/app/services/quran-api-service.service.ts
@@ -8,14 +8,17 @@ import { Observable } from 'rxjs';
 export class QuranApiServiceService {
 
   constructor(private http : HttpClient) { }
-  //private apiUrl = 'https://quranapi.pages.dev/api';
+  private apiUrl1 = 'https://quranapi.pages.dev/api';
   private apiUrl = 'https://api.quranhub.com/v1/ayah';
-  private options = {method: 'GET', headers: {accept: 'application/json'}};
+  //private options = {method: 'GET', headers: {accept: 'application/json'}};
 
   // getAyah(ayahNumber: number, surahNumber: number): Observable<any> {
   //   return this.http.get(`${this.apiUrl}/${surahNumber}/${ayahNumber}.json`);
   // }
+  getAyahTranslation(surahNumber: number,ayahNumber: number): Observable<any> {
+    return this.http.get(`${this.apiUrl1}/${surahNumber}/${ayahNumber}.json`);
+  }
   getAyah(surahNumber: number,ayahNumber: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/${surahNumber}:${ayahNumber}`,this.options);
+    return this.http.get(`${this.apiUrl}/${surahNumber}:${ayahNumber}`);
   }
 }
